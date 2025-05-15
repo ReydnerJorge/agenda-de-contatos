a5d7d0c33272fc7f777e2d0f55661c4af2c6c7b6
Reydner Jorge <reydneroliveirajorge96@gmail.com>
2025-05-14 21:37:02 -0300

    fix: correção de bug nos arquivos criados anteriormente.

--- src/contatc_agenda/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/contact_agenda/main.py
-# src/contact_agenda/__init__.py
-
-__all__ = ["menu", "handlers", "storage", "models"]
--- src/contatc_agenda/main.py
+++
@@ -1,9 +0,0 @@
-# src/contact_agenda/main.py
-
-from contact_agenda.menu import show_menu
-
-def main():
-    show_menu()
-
-if __name__ == "__main__":
-    main()
